fix custom latent loss and add log-softmax before KLD loss

text-to-face-generation/loss.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.utils.data
 import torchvision.transforms as transforms
 
@@ -14,29 +15,42 @@ class ReconstructionLoss(nn.Module):
         d = (0,1) if len(recon_x.shape) == 2 else (0,1,2)
         return torch.sum(self.mse_loss(recon_x, x), dim=d)
 
+# KL-divergence loss
+class KLDLoss(nn.Module):
+    """KL-Divergence loss with log softmax"""
+    def __init__(self, reduction='batchmean'):
+        super(KLDLoss, self).__init__()
+        self.kld_loss_fn = nn.KLDivLoss(reduction=reduction)
+    def forward(self, output, target):
+        return self.kld_loss_fn(F.log_softmax(output, dim=2), F.softmax(target, dim=2))
+
 # latent space vector loss
 class LatentLoss(nn.Module):
     """
     Custom loss for latent space vectors.
-    One or a combination of :
+    Can be one or a combination of the following losses :
         - L1Loss (l1).
         - MSELoss (mse).
         - KLDivLoss (kl).
         - Cosine Similarity (cosine).
     """
-    def __init__(self, loss_list, reduction='mean'):
+    def __init__(self, losses_list, reduction='mean'):
         super(LatentLoss, self).__init__()
-        self.losses = list()
-        if 'l1' in loss_list:
-            self.losses.append(nn.L1Loss(reduction=reduction))
-        if 'mse' in loss_list:
-            self.losses.append(nn.MSELoss(reduction=reduction))
-        if 'kl' in loss_list:
-            self.losses.append(nn.KLDivLoss(reduction=reduction))
-        if 'cosine' in loss_list:
-            self.losses.append(nn.CosineSimilarity(dim=1))
+        self.losses_fn = list()
+        if 'l1' in losses_list:
+            self.losses_fn.append(nn.L1Loss(reduction=reduction))
+        if 'mse' in losses_list:
+            self.losses_fn.append(nn.MSELoss(reduction=reduction))
+        if 'kl' in losses_list:
+            if reduction == 'mean':
+                self.losses_fn.append(KLDLoss(reduction='batchmean'))
+            else:
+                self.losses_fn.append(KLDLoss(reduction=reduction))
+        if 'cosine' in losses_list:
+            self.losses_fn.append(nn.CosineSimilarity(dim=2))
+
     def forward(self, output, target):
-        total_loss = self.losses[0](output, target)
-        for idx in range(1, len(self.losses)):
-            total_loss += self.losses[idx](output, target)
+        total_loss = self.losses_fn[0](output, target)
+        for idx in range(1, len(self.losses_fn)):
+            total_loss += self.losses_fn[idx](output, target)
         return total_loss