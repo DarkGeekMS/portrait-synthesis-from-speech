6f88f6f68eec6a2836388acbff167a807b7e2214
monda <remondatalaat21@gmail.com>
2020-10-30 15:04:19 +0200

    attentive model

--- text-to-face-generation/infersent.py
+++ text-to-face-generation/infersent.py
@@ -1,49 +1,152 @@
+from __future__ import print_function
 import torch
+from torch.autograd import Variable
 import torch.nn as nn
+import os
 
-import numpy as np
-import time
+"""
+configuration parameters:
+ntoken
+ninp
+nlayers
+enc_lstm_dim
+dictionary
+word_vector
+dpout_model
+pool_type
+nfc
+attention_hops
+attention_unit
+class_number
+"""
 
-class InferSent(nn.Module):
-    """Infersent sentence embedding model"""
+class BiLSTM(nn.Module):
+
     def __init__(self, config):
-        # initialize infersent model
-        super(InferSent, self).__init__()
-        self.word_emb_dim = config['word_emb_dim']
-        self.enc_lstm_dim = config['enc_lstm_dim']
-        self.pool_type = config['pool_type']
-        self.dpout_model = config['dpout_model']
-        self.max_pad = config['max_pad']
+        super(BiLSTM, self).__init__()
+        self.drop = nn.Dropout(config['dpout_model'])
+        self.encoder = nn.Embedding(config['ntoken'], config['ninp'])
+        self.bilstm = nn.LSTM(config['ninp'], config['enc_lstm_dim'], config['nlayers'], dropout=config['dpout_model'],
+                              bidirectional=True)
+        self.nlayers = config['nlayers']
+        self.nhid = config['enc_lstm_dim']
+        self.pooling = config['pool_type']
+        self.dictionary = config['dictionary']
+#        self.init_weights()
+        self.encoder.weight.data[self.dictionary.word2idx['<pad>']] = 0
+        if os.path.exists(config['word_vector']):
+            print('Loading word vectors from', config['word_vector'])
+            vectors = torch.load(config['word_vector'])
+            assert vectors[2] >= config['ninp']
+            vocab = vectors[0]
+            vectors = vectors[1]
+            loaded_cnt = 0
+            for word in self.dictionary.word2idx:
+                if word not in vocab:
+                    continue
+                real_id = self.dictionary.word2idx[word]
+                loaded_id = vocab[word]
+                self.encoder.weight.data[real_id] = vectors[loaded_id][:config['ninp']]
+                loaded_cnt += 1
+            print('%d words from external word vectors loaded.' % loaded_cnt)
 
-        self.enc_lstm = nn.LSTM(self.word_emb_dim, self.enc_lstm_dim, 1,
-                                bidirectional=True, dropout=self.dpout_model)
+    # note: init_range constraints the value of initial weights
+    def init_weights(self, init_range=0.1):
+        self.encoder.weight.data.uniform_(-init_range, init_range)
 
-    def is_cuda(self):
-        # either all weights are on cpu or they are on gpu
-        return self.enc_lstm.bias_hh_l0.data.is_cuda
+    def forward(self, inp, hidden):
+        emb = self.drop(self.encoder(inp))
+        outp = self.bilstm(emb, hidden)[0]
+        if self.pooling == 'mean':
+            outp = torch.mean(outp, 0).squeeze()
+        elif self.pooling == 'max':
+            outp = torch.max(outp, 0)[0].squeeze()
+        elif self.pooling == 'all' or self.pooling == 'all-word':
+            outp = torch.transpose(outp, 0, 1).contiguous()
+        return outp, emb
 
-    def forward(self, sent_tuple):
-        # sent_tuple: (sent, sent_len)
-        # sent_len: [max_len, ..., min_len] (bsize)
-        # sent: (seqlen x bsize x worddim)
-        sent, sent_len = sent_tuple
+    def init_hidden(self, bsz):
+        weight = next(self.parameters()).data
+        return (Variable(weight.new(self.nlayers * 2, bsz, self.nhid).zero_()),
+                Variable(weight.new(self.nlayers * 2, bsz, self.nhid).zero_()))
 
-        # Handling padding in Recurrent Networks
-        sent_packed = nn.utils.rnn.pack_padded_sequence(sent, sent_len)
-        sent_output = self.enc_lstm(sent_packed)[0]  # seqlen x batch x 2*nhid
-        sent_output = nn.utils.rnn.pad_packed_sequence(sent_output)[0]
 
-        # Pooling
-        if self.pool_type == "mean":
-            sent_len = torch.FloatTensor(sent_len.copy()).unsqueeze(1).cuda()
-            emb = torch.sum(sent_output, 0).squeeze(0)
-            emb = emb / sent_len.expand_as(emb)
-        elif self.pool_type == "max":
-            if not self.max_pad:
-                sent_output[sent_output == 0] = -1e9
-            emb = torch.max(sent_output, 0)[0]
-            if emb.ndimension() == 3:
-                emb = emb.squeeze(0)
-                assert emb.ndimension() == 2
+class SelfAttentiveEncoder(nn.Module):
 
-        return emb
+    def __init__(self, config):
+        super(SelfAttentiveEncoder, self).__init__()
+        self.bilstm = BiLSTM(config)
+        self.drop = nn.Dropout(config['dpout_model'])
+        self.ws1 = nn.Linear(config['enc_lstm_dim'] * 2, config['attention_unit'], bias=False)
+        self.ws2 = nn.Linear(config['attention_unit'], config['attention_hops'], bias=False)
+        self.tanh = nn.Tanh()
+        self.softmax = nn.Softmax()
+        self.dictionary = config['dictionary']
+#        self.init_weights()
+        self.attention_hops = config['attention_hops']
+
+    def init_weights(self, init_range=0.1):
+        self.ws1.weight.data.uniform_(-init_range, init_range)
+        self.ws2.weight.data.uniform_(-init_range, init_range)
+
+    def forward(self, inp, hidden):
+        outp = self.bilstm.forward(inp, hidden)[0]
+        size = outp.size()  # [bsz, len, nhid]
+        compressed_embeddings = outp.view(-1, size[2])  # [bsz*len, nhid*2]
+        transformed_inp = torch.transpose(inp, 0, 1).contiguous()  # [bsz, len]
+        transformed_inp = transformed_inp.view(size[0], 1, size[1])  # [bsz, 1, len]
+        concatenated_inp = [transformed_inp for i in range(self.attention_hops)]
+        concatenated_inp = torch.cat(concatenated_inp, 1)  # [bsz, hop, len]
+
+        hbar = self.tanh(self.ws1(self.drop(compressed_embeddings)))  # [bsz*len, attention-unit]
+        alphas = self.ws2(hbar).view(size[0], size[1], -1)  # [bsz, len, hop]
+        alphas = torch.transpose(alphas, 1, 2).contiguous()  # [bsz, hop, len]
+        penalized_alphas = alphas + (
+            -10000 * (concatenated_inp == self.dictionary.word2idx['<pad>']).float())
+            # [bsz, hop, len] + [bsz, hop, len]
+        alphas = self.softmax(penalized_alphas.view(-1, size[1]))  # [bsz*hop, len]
+        alphas = alphas.view(size[0], self.attention_hops, size[1])  # [bsz, hop, len]
+        return torch.bmm(alphas, outp), alphas
+
+    def init_hidden(self, bsz):
+        return self.bilstm.init_hidden(bsz)
+
+
+class Classifier(nn.Module):
+
+    def __init__(self, config):
+        super(Classifier, self).__init__()
+        if config['pool_type'] == 'mean' or config['pool_type'] == 'max':
+            self.encoder = BiLSTM(config)
+            self.fc = nn.Linear(config['enc_lstm_dim'] * 2, config['nfc'])
+        elif config['pool_type'] == 'all':
+            self.encoder = SelfAttentiveEncoder(config)
+            self.fc = nn.Linear(config['enc_lstm_dim'] * 2 * config['attention_hops'], config['nfc'])
+        else:
+            raise Exception('Error when initializing Classifier')
+        self.drop = nn.Dropout(config['dpout_model'])
+        self.tanh = nn.Tanh()
+        self.pred = nn.Linear(config['nfc'], config['class_number'])
+        self.dictionary = config['dictionary']
+#        self.init_weights()
+
+    def init_weights(self, init_range=0.1):
+        self.fc.weight.data.uniform_(-init_range, init_range)
+        self.fc.bias.data.fill_(0)
+        self.pred.weight.data.uniform_(-init_range, init_range)
+        self.pred.bias.data.fill_(0)
+
+    def forward(self, inp, hidden):
+        outp, attention = self.encoder.forward(inp, hidden)
+        outp = outp.view(outp.size(0), -1)
+        fc = self.tanh(self.fc(self.drop(outp)))
+        pred = self.pred(self.drop(fc))
+        if type(self.encoder) == BiLSTM:
+            attention = None
+        return pred, attention
+
+    def init_hidden(self, bsz):
+        return self.encoder.init_hidden(bsz)
+
+    def encode(self, inp, hidden):
+        return self.encoder.forward(inp, hidden)[0]
