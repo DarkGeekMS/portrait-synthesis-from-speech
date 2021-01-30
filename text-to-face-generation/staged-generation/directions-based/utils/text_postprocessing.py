562de4eeae63b404357f16846702cf05b3949741
DarkGeekMS <mohamedshawky911@gmail.com>
2021-01-30 23:32:52 +0200

    post-process text logits for more facial attributes

--- text-to-face-generation/staged-generation/directions-based/utils/text_postprocessing.py
+++ text-to-face-generation/staged-generation/directions-based/utils/text_postprocessing.py
@@ -5,11 +5,38 @@ def postprocess_text_logits(sent_pred, axes_range):
     # in order to align indices and scale logits
     # initialize processed logits array with -1
     proc_pred = np.array([-1.0]*32)
-    # [18] face thickness attributes
+    # [0] bushy eyebrows attribute
+    proc_pred[0] = sent_pred[1]
+    # [1,2,3,4] hair color attributes
+    if sent_pred[2] != -1:
+        # black hair
+        # NOTE : navigate in negative brown direction
+        proc_pred[3] = 0
+    else:
+        # set hair color with output logits
+        proc_pred[1] = sent_pred[3]
+        proc_pred[2] = sent_pred[4]
+        proc_pred[3] = sent_pred[5]
+        proc_pred[4] = sent_pred[6]
+    # [5] straight/curly hair attribute
+    proc_pred[5] = sent_pred[7]
+    # [6] receding hairline attribute
+    proc_pred[6] = sent_pred[8]
+    # [7] bald attribute
+    proc_pred[7] = sent_pred[9]
+    # [8] hair with bangs attribute
+    proc_pred[8] = sent_pred[10]
+    # [9] hair color attribute
+    proc_pred[9] = sent_pred[11]
+    # [10] beard attribute
+    proc_pred[10] = sent_pred[14]
+    # [11] skin color attribute
+    proc_pred[11] = sent_pred[16]
+    # [13] face thickness attribute
     proc_pred[13] = sent_pred[18]
-    # [19] gender attributes
+    # [14] gender attribute
     proc_pred[14] = sent_pred[19]
-    # [20] age attributes
+    # [15] age attribute
     proc_pred[15] = sent_pred[20]
     # re-scale all attributes based on considered axes range
     proc_pred_scaled = np.array(
