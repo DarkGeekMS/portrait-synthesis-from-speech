4c9e0345eca38cf52a1c458f13d3b1d84d4cfea5
DarkGeekMS <mohamedshawky911@gmail.com>
2021-04-03 12:09:43 +0200

    extract stylegan2 synthesis network only to reduce memory footprint

--- text-to-face-generation/staged-generation/directions-based/generate_faces.py
+++ text-to-face-generation/staged-generation/directions-based/generate_faces.py
@@ -1,9 +1,10 @@
-from stylegan2_generator import StyleGAN2Generator
+from stylegan2_networks import SynthesisNetwork
 from text_processing.inference import BERTMultiLabelClassifier
 from utils.seed_generation import generate_seed
 from utils.latent_manipulation import manipulate_latent
 from utils.text_postprocessing import postprocess_text_logits
 
+import torch
 import numpy as np
 import skimage.io as io
 import coloredlogs
@@ -15,6 +16,9 @@ import os
 
 def generate_faces(config):
     # generate face images from text descriptions using directions-based latent navigation
+    # initialize CUDA device
+    device = torch.device('cuda')
+
     # initialize debug logger
     logger = logging.getLogger(__name__)
     coloredlogs.install(level='DEBUG', logger=logger)
@@ -33,9 +37,9 @@ def generate_faces(config):
 
     # initialize StyleGAN2 generator
     print('Initializing StyleGAN2 generator ...\n')
-    stylegan2_generator = StyleGAN2Generator(
-        config['stylegan_pkl'], truncation_psi=config['truncation_psi'], use_projector=False
-    )
+    stylegan2_generator = SynthesisNetwork(w_dim=512, img_resolution=1024, img_channels=3)
+    stylegan2_generator.load_state_dict(torch.load(config['stylegan_pt']))
+    stylegan2_generator.to(device)
 
     # read pre-defined feature directions
     print('Reading feature directions ...\n')
@@ -70,8 +74,14 @@ def generate_faces(config):
             for logit_idx in config['debug_features']:
                 logger.debug(f'[{logit_idx}] : {image_logits[logit_idx]}')
             logger.debug('Saving initial face image ...')
-            initial_face_image = stylegan2_generator.generate_images(np.expand_dims(latent_vector, axis=0))
-            io.imsave(f'results/{idx}_init.png', initial_face_image[0])
+            w_tensor = torch.tensor(np.expand_dims(latent_vector, axis=0), device=device)
+            initial_face_image = stylegan2_generator(w_tensor, noise_mode='const')
+            initial_face_image = initial_face_image.permute(0, 2, 3, 1).cpu().detach().numpy()[0]
+            initial_face_image[initial_face_image < -1.0] = -1.0
+            initial_face_image[initial_face_image > 1.0] = 1.0
+            initial_face_image = (initial_face_image + 1.0) * 127.5
+            initial_face_image = initial_face_image.astype(np.uint8)
+            io.imsave(f'results/{idx}_init.png', initial_face_image)
         
         # manipulate latent space to get the target latent vector
         print('Performing latent manipulation ...')
@@ -94,11 +104,17 @@ def generate_faces(config):
         
         # generate the required face image
         print('Performing face generation ...')
-        face_image = stylegan2_generator.generate_images(target_latent)
+        w_tensor = torch.tensor(target_latent, device=device)
+        face_image = stylegan2_generator(w_tensor, noise_mode='const')
+        face_image = face_image.permute(0, 2, 3, 1).cpu().detach().numpy()[0]
+        face_image[face_image < -1.0] = -1.0
+        face_image[face_image > 1.0] = 1.0
+        face_image = (face_image + 1.0) * 127.5
+        face_image = face_image.astype(np.uint8)
 
         # save the generated face image
         print('Saving output face image ...')
-        io.imsave(f'results/{idx}.png', face_image[0])
+        io.imsave(f'results/{idx}.png', face_image)
         print('\n-------------------------------------------------------------\n')
 
 
